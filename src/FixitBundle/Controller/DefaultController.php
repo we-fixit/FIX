26fc0714dd73db56438eae521f20c4f83c49697d
Ismail <ismail.abdennadher@esprit.tn>
2019-02-13 19:23:41 +0100

    initial commit
    Signed-off-by: Ismail <ismail.abdennadher@esprit.tn>

--- src/FixitBundle/Controller/DefaultController.php
+++ src/FixitBundle/Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('@Fixit/Default/index.html.twig');
     }
+
+    public function helloword()
+    {
+        return;
+    }
 }
